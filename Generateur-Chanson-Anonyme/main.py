5d589a05ca0f719ac0f184f7b70d01a3a2bdde8e
NX-GoldJust <117308582+NX-GoldJust@users.noreply.github.com>
2023-04-29 16:05:38 +0200

    Debugage

--- Generateur-Chanson-Anonyme/main.py
+++ Generateur-Chanson-Anonyme/main.py
@@ -1,11 +1,13 @@
 from synonymes import linternaute
-parole = input("Entrez vos paroles ici :\n")
+"""parole = input("Entrez vos paroles ici :\n")
 new = []
 
 for mot in parole.split(" "):
+    print(mot)
     new_mot = linternaute(mot)
+    print(new_mot)
     if len(new_mot)<1:
         new.append(mot)
     else:
         new.append(new_mot[0])
-print("".join(new))
+print(" ".join(new))"""
