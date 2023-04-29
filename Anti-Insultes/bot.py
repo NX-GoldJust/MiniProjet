0a018930bee4322f151826c624ac192dc7a52bbb
NX-GoldJust <117308582+NX-GoldJust@users.noreply.github.com>
2023-04-29 16:55:26 +0200

    Liste mot interdit

--- Anti-Insultes/bot.py
+++ Anti-Insultes/bot.py
@@ -3,3 +3,4 @@ from discord.ext import commands
 
 bot = commands.Bot("!", intents=discord.Intents.all())
 
+mot_interdit = ["tg", "nique", 'ta mère', 'ta mere', 'enculé', "encule", 'pd', 'gay', "con", 'connard', "pute", 'saloppe', "salope"]
